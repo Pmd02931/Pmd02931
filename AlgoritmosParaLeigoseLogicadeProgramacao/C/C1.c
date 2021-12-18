2401ccd6ecb2728bca4d68696113fe723964530e
Pmd02931 <79956869+Pmd02931@users.noreply.github.com>
2021-12-18 12:35:58 -0300

    Up

--- AlgoritmosParaLeigoseLogicadeProgramacao/C/C1.c
+++ AlgoritmosParaLeigoseLogicadeProgramacao/C/C1.c
@@ -5,16 +5,16 @@ int main()
     int vector[5], pairs[5], cont_p = 0;
     for (int i = 0; i < 5; i++)
     {
-        printf("Enter a value: ");
+        printf("Enter a value(%d/5): ", i + 1);
         scanf("%d", &vector[i]);
-        if (vector[i] > 0)
+        if (vector[i] > 0 && vector[i] % 2 == 0)
         {
             pairs[cont_p] = vector[i];
-            cont_p = cont_p++;
+            cont_p++;
         }
     }
     for (int i = 0; i < cont_p; i++)
     {
-        printf("%d\n", pares[i]);
+        printf("%d\n", pairs[i]);
     }
 }
