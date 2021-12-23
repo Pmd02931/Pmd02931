aaadda91e997dc7c4dab6bc83ae9aa843393c1e2
Pmd02931 <79956869+Pmd02931@users.noreply.github.com>
2021-12-23 11:32:58 -0300

    del

--- AlgoritmosParaLeigoseLogicadeProgramacao/C/C1.c
+++
@@ -1,9 +0,0 @@
-//Make an algorithm to add two numbers and multiply the result by first number//
-int main(){
-    int a, b;
-    printf("Enter the first number");
-    scanf("%d", &a);
-    printf("Enter the second number");
-    scanf("%d", &b);
-    printf("The number %d multiplied by the sum of number %d and %d gives: %d ", a, a, b, (a+b)*a);
-}
